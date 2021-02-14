ccd20d870445f0379ed7db25a2c87c2dd7641196
Neil Faiman <neil.git@faiman.org>
2021-02-14 18:30:49 -0500

    Initial Commit

---
+++ PersistentIncludeParameters/PersistentIncludeParameters.swift
@@ -0,0 +1,133 @@
+//
+//  PersistentIncludeParameters.swift
+//  PersistentIncludeParameters
+//
+//  Created by Neil Faiman on 7/17/20.
+//  Copyright © 2020 Neil Faiman. All rights reserved.
+//
+
+import Foundation
+
+/// Access the parameters from a script or program called from a BBEdit
+/// persistent include construct.
+///
+/// BBEdit is the popular Mac text editor from
+/// [Barebones Software](www.barebnes.com).
+///
+/// A “persistent include” in a BBEdit HTML file is a pair of comments:
+///
+///     <!-- #bbinclude "include file" [ #name#="value" ]... -->
+///     ...
+///     <!-- end bbinclude -->
+///
+/// When the HTML file is updated, BBEdit replaces the text between the
+/// comments. If the `include file` is a text file, then the replacement text
+/// is a copy of the include file, with  occurrences of the `#name#` strings
+/// replaced with the corresponding `value` strings from the `bbinclude`
+/// comment. If the include file is an executable, then BBEdit runs it, passing
+/// the file path of the including HTML file as the first command line argument,
+/// and the parameter names and values as additional arguments. The standard
+/// output from the include file execution becomes the replacememt text.
+///
+/// This `struct` provides a consistent way of accessing the parameters in
+/// a Swift script that is used as the `include file` of a persistent include.
+/// To use it, just declare one:
+///
+///         let params = PersistentIncludeParameters()
+///
+/// Then
+///
+///  - `params.script` is the path to the script file.
+///  - `params.includer` is the path to the HTML file that contains the
+///     include directive.
+///  - `params.count` is the number of `#name#="value"` pairs in the
+///    include directive.
+///  - If the include directive specified `#name#="value"`, then
+///       `params["name"]` returns `"value"`.
+///
+public struct PersistentIncludeParameters {
+    
+    /// The file path of the script.
+    public let script : String
+    
+    /// The file path of the including HTML file.
+    public let includer: String
+    
+    /// Values of named parameters.
+    ///
+    /// persistentIncludeParameters["name"] == "value" if the persistent
+    /// include contains the parameter `#name#="value"`.
+    ///
+    /// - Parameter name: A string that was specified as a parameter name
+    ///   (enclosed in # marks) in the persistent include.
+    ///
+    /// - Returns: The string that was specified as the value of the parameter
+    ///    with the specified name, or `nil` if the include did not have a
+    ///    parameter with the specified name.
+    ///
+    /// - Note: Parameter names are case-insensitive.
+    ///
+    public subscript(name: String) -> String? {
+        return parameters[name.uppercased()]
+    }
+    
+    /// The number of parameters.
+    public var count: Int {
+        return parameters.count
+    }
+    
+    private let parameters: [String: String]
+    
+    /// Error type thrown if an initializer argument array doesn’t
+    /// match the expected format.
+    public struct ArgumentsError : Error {
+        /// Description of the error.
+        public let errorText: String
+    }
+    
+    /// Initialize from the commmand line arguments.
+    ///
+    /// - Throws: ArgumentsError
+    ///
+    public init() throws {
+        try self.init(arguments: CommandLine.arguments)
+    }
+    
+    /// Initialize from an array of arguments (beginning with the path to the
+    /// script or program).
+    ///
+    /// - Throws: ArgumentsError
+    ///
+    public init(arguments: [String]) throws {
+        guard arguments.count >= 2 else {
+            throw ArgumentsError(errorText: """
+                Argument array must contain the script and includer \
+                file paths
+                """)
+        }
+        script = arguments[0]
+        includer = arguments[1].replacingOccurrences(of: "\\ ", with: " ")
+        guard arguments.count % 2 == 0 else {
+            throw ArgumentsError(errorText:
+                "Argument array must contain matched name / value pairs")
+        }
+        let keys = stride(from: 2, to: arguments.count, by: 2)
+            .map { arguments[$0].uppercased() }
+        let values = stride(from: 3, to: arguments.count, by: 2)
+            .map { arguments[$0] }
+        try parameters = [String:String](zip(keys, values)) { _, _ in
+            throw ArgumentsError(errorText:
+                "Two parameters have the same name")
+        }
+    }
+
+    /// Initializee from a list of arguments (beginning with the path to the
+    /// script or program).
+    ///
+    /// - Throws: ArgumentsError
+    ///
+    public init(arguments: String...) throws {
+        try self.init(arguments: arguments)
+    }
+
+}
---
+++ PersistentIncludeParametersTests/PersistentIncludeParametersTests.swift
@@ -0,0 +1,120 @@
+//
+//  PersistentIncludeParametersTests.swift
+//  PersistentIncludeParametersTests
+//
+//  Created by Neil Faiman on 7/18/20.
+//  Copyright © 2020 Neil Faiman. All rights reserved.
+//
+
+import XCTest
+import PersistentIncludeParameters
+
+class PersistentIncludeParametersTests: XCTestCase {
+
+    func testNoParameters() {
+        let params: PersistentIncludeParameters
+        do {
+            params = try PersistentIncludeParameters(arguments:
+                "scriptPath", "includerPath")
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+        XCTAssertEqual(params.script, "scriptPath")
+        XCTAssertEqual(params.includer, "includerPath")
+        XCTAssertEqual(params.count, 0)
+        XCTAssertNil(params["foo"])
+    }
+    
+    func testOneParameter() {
+        let params: PersistentIncludeParameters
+        do {
+            params = try PersistentIncludeParameters(arguments:
+                "scriptPath", "includerPath", "FOO", "foobar")
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+        XCTAssertEqual(params.script, "scriptPath")
+        XCTAssertEqual(params.includer, "includerPath")
+        XCTAssertEqual(params.count, 1)
+        XCTAssertEqual(params["foo"], "foobar")
+        XCTAssertEqual(params["FOO"], "foobar")     // case insensitive
+        XCTAssertNil(params["baz"])
+    }
+
+    func testThreeParameters() {
+        let params: PersistentIncludeParameters
+        do {
+            params = try PersistentIncludeParameters(arguments:
+                "scriptPath", "includerPath", "FOO1", "BAR1",
+                              "foo2", "bar2", "foo3", "bar3")
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+        XCTAssertEqual(params.script, "scriptPath")
+        XCTAssertEqual(params.includer, "includerPath")
+        XCTAssertEqual(params.count, 3)
+        XCTAssertEqual(params["foo1"], "BAR1")
+        XCTAssertEqual(params["FOO2"], "bar2")
+        XCTAssertEqual(params["foo3"], "bar3")
+    }
+    
+    func testTooFewArguments() {
+        do {
+            let _ = try PersistentIncludeParameters(arguments:
+                [])
+        } catch let error as PersistentIncludeParameters.ArgumentsError {
+            XCTAssertEqual(error.errorText, """
+                           Argument array must contain the script and includer \
+                           file paths
+                           """)
+            return
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+        do {
+            let _ = try PersistentIncludeParameters(arguments: "scriptPath")
+        } catch let error as PersistentIncludeParameters.ArgumentsError {
+            XCTAssertEqual(error.errorText, """
+                           Argument array must contain the script and includer \
+                           file paths
+                           """)
+            return
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+    }
+    
+    func testOddArgumentCount() {
+        do {
+            let _ = try PersistentIncludeParameters(arguments:
+                "scriptPath", "includerPath", "FOO")
+        } catch let error as PersistentIncludeParameters.ArgumentsError {
+            XCTAssertEqual(error.errorText,
+                    "Argument array must contain matched name / value pairs")
+            return
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+    }
+
+    func testDuplicateParameter() {
+        do {
+            let _ = try PersistentIncludeParameters(arguments:
+                "scriptPath", "includerPath", "FOO", "bar", "foo", "baz")
+        } catch let error as PersistentIncludeParameters.ArgumentsError {
+            XCTAssertEqual(error.errorText,
+                    "Two parameters have the same name")
+            return
+        } catch {
+            XCTFail("Unexpected error \(error) hrown from PersistentIncludeParameters initializer")
+            return
+        }
+    }
+
+}
